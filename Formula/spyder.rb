5717355d51e688f80f4ce771b0d006e07a1e91a1
Bob Berry <bob@cocoon-ai.com>
2026-01-06 13:18:17 -0800

    Update to v0.2.0, use CloudFront CDN

--- Formula/spyder.rb
+++ Formula/spyder.rb
@@ -1,22 +1,22 @@
 class Spyder < Formula
   desc "CLI for Spyder agent messaging"
   homepage "https://github.com/Cocoon-AI/spyder"
-  version "0.1.0"
+  version "0.2.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://cocoon-ai-releases.s3.us-west-2.amazonaws.com/spyder/0.1.0/spyder-0.1.0-darwin-arm64"
-      sha256 "c82c85a0195dc56d9c23f7654cd1d430999f6f375990b3224871102775939fff"
+      url "https://dbe7177numv5y.cloudfront.net/spyder/0.2.0/spyder-0.2.0-darwin-arm64"
+      sha256 "03f9cc736575a5ae0eca3dae1b8e8afd9b27310c9b692be9261bd57230be123b"
     else
-      url "https://cocoon-ai-releases.s3.us-west-2.amazonaws.com/spyder/0.1.0/spyder-0.1.0-darwin-amd64"
-      sha256 "dae9e18ce048b7d5c40de4fee4763eea97775b4c460e32cce290ad17a6fe9f7b"
+      url "https://dbe7177numv5y.cloudfront.net/spyder/0.2.0/spyder-0.2.0-darwin-amd64"
+      sha256 "f418a418eb359f8d9ba4722d752b20d0d4409d74b5cce98f6a34a2a7e6d84fa9"
     end
   end
 
   on_linux do
-    url "https://cocoon-ai-releases.s3.us-west-2.amazonaws.com/spyder/0.1.0/spyder-0.1.0-linux-amd64"
-    sha256 "f6a85af96e493f4906a6a8a20553073ac7040c710e46ab1e38d77b7e00b53412"
+    url "https://dbe7177numv5y.cloudfront.net/spyder/0.2.0/spyder-0.2.0-linux-amd64"
+    sha256 "08df729e1b9624162c6ea59d4ffbb055ec4c193342477e7ca5cb0926e62c6a03"
   end
 
   def install
