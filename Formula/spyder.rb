6cc45ac835c53c00ab53ab74f2f0866b88c369f0
Bob Berry <bob@cocoon-ai.com>
2026-01-06 14:10:51 -0800

    Use dl.cocoon-ai.com for downloads

--- Formula/spyder.rb
+++ Formula/spyder.rb
@@ -6,16 +6,16 @@ class Spyder < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://dbe7177numv5y.cloudfront.net/spyder/0.2.2/spyder-0.2.2-darwin-arm64"
+      url "https://dl.cocoon-ai.com/spyder/0.2.2/spyder-0.2.2-darwin-arm64"
       sha256 "5f5cd5d0792589706748fd3350c3bec11e72e4a0b8b5eccdd2d30dd48fc41276"
     else
-      url "https://dbe7177numv5y.cloudfront.net/spyder/0.2.2/spyder-0.2.2-darwin-amd64"
+      url "https://dl.cocoon-ai.com/spyder/0.2.2/spyder-0.2.2-darwin-amd64"
       sha256 "a117efe1ae0534cfdb3e9ebe3372c47e802534adfeaccd6c3b08d2007a24d801"
     end
   end
 
   on_linux do
-    url "https://dbe7177numv5y.cloudfront.net/spyder/0.2.2/spyder-0.2.2-linux-amd64"
+    url "https://dl.cocoon-ai.com/spyder/0.2.2/spyder-0.2.2-linux-amd64"
     sha256 "ff7dde8c08b82efed0ef5f62f7e843279ed413d97760714c70184cea9c9eb74f"
   end
 
